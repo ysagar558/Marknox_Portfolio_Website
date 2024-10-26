2abc6c72137c94dac69848d3f65ca5089bee21e6
ysagar558 <90234181+ysagar558@users.noreply.github.com>
2024-10-26 23:00:52 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,116 @@
+const logotext = "SAGAR YADAV";
+const meta = {
+    title: "Sagar Yadav",
+    description: "I’m Sagar Yadav Web devloper",
+};
+
+const introdata = {
+    title: "I’m Sagar Yadav",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop attracting websites",
+    },
+    description: "I am Sagar Radhakishan Yadav graduated from SGGSIE&T,Nanded in Information Technology stream",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am basically from Selu Dist Parbhani Maharashtra. I have completed my graduation in B.Tech in Information Technology stream. I have good knowledge of technologies such as HTML, CSS, Javascript, ReactJS, Java, MySQL, PHP, C , C++ and many more",
+};
+const worktimeline = [{
+         },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 98,
+    },
+    {
+        name: "CSS",
+        value: 97,
+    },
+    {
+        name: "Javascript",
+        value: 97,
+    },
+    {
+        name: "ReactJS",
+        value: 96,
+    },
+    {
+        name: "Java",
+        value: 95,
+    },
+    {
+        name: "MySQL",
+        value: 98,
+    },
+    {
+        name: "C Programming Language",
+        value: 99,
+    },
+];
+
+const services = [{
+        title: "Frontend Development",
+        description: "I have good knowledge of frontend technologies such as HTML,CSS, Javascript.",
+    },
+    {
+        title: "Backend Development",
+        description: "I have good knowledge of backend technologies such as MySQL, PHP.",
+    },
+    {
+        title: "Other Services",
+        description: "I am good comunication skills. I can collaborate with anyone",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Developed college sports event website in HTML.CSS. Javascript.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400?grayscale",
+        description: "Developed Hotel Mangement System in PHP & MySQL.",
+        link: "#",
+    },
+    
+    {
+        img: "https://picsum.photos/400?grayscale",
+        description: "Developed ATM Mangement System in Java & MySQL.",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ysagar558@gmail.com",
+    YOUR_FONE: "(+91)9881970055",
+    description: "Please feel free to contact me using above details at any time. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ysagar558",
+    facebook: "https://www.facebook.com/profile.php?id=100013406237165",
+    linkedin: "https://www.linkedin.com/in/sagar-yadav-a255b51a9",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
